fe62fc760487d38a77e3a702539d1becdfd1d4b6
Ben Tyler <benjamin.tyler@gmail.com>
2015-07-08 22:45:38 +0200

    trivial approach; not working; units won't spawn.

---
+++ modinfo.lua
@@ -0,0 +1,14 @@
+return {
+  name='Scenario Editor S44',
+  description='Scenario Editor for Spring: 1944',
+  shortname='SE S44',
+  version='git',
+  mutator='Official',
+  game='Scenario Editor S44',
+  shortGame='SE S44',
+  modtype=1,
+  depend = {  			
+  	'Spring: 1944 $VERSION',
+  	'Scenario Editor Core git',
+  },
+}
