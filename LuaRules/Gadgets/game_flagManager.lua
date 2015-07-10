f57a7d1b2ffa4a4504b90fb6dba927d827f42493
Ben Tyler <benjamin.tyler@gmail.com>
2015-07-10 21:17:56 +0200

    Disable some crashing gadgets.
    Not sure if this is the 'scened way' -- turning off gameplay oriented gadgets
    that theoretically only matter during a multiplayer match. If not, I'll see
    about fixing them to work when running under scened.

---
+++ LuaRules/Gadgets/game_flagManager.lua
@@ -0,0 +1,11 @@
+function gadget:GetInfo()
+	return {
+		name	  = "Flag Manager",
+		desc	  = "Populates maps with flags and handles control",
+		author	  = "FLOZi, AnalyseMetalMap algorithm from easymetal.lua by CarRepairer",
+		date	  = "31st July 2008",
+		license   = "GNU GPL v2",
+		layer	  = -5,
+		enabled   = false
+	}
+end
---
+++ LuaRules/Gadgets/game_handleFlagReturns.lua
@@ -0,0 +1,11 @@
+function gadget:GetInfo()
+	return {
+		name = "Flag Returns",
+		desc = "Set flags production in map_command_per_player mode, increases the output of flags according to how long they've been held, set init_production and production rules params",
+		author = "Nemo (B. Tyler), FLOZi (C. Lawrence), Prospector widget code form gui_prospector.lua by Evil4Zerggin, Szunti",
+		date = "2011-09-01",
+		license = "GNU GPL v2",
+		layer = 0,
+		enabled = false
+	}
+end
